570eace6b8c9ff591a350c56c6c2150c046424fb
Fokrul Islam Mehedi <fokrulislammehedi2000@gmail.com>
2021-08-19 12:44:06 +0600

    [ADD] sleep_until function added

--- schedule_task.py
+++ schedule_task.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import schedule
 from module.bot import Bot, config
 
@@ -15,11 +16,24 @@ schedule.every().monday.at(SCHEDULE_TIME).do(send_message, config['MON_ROUTINE']
 schedule.every().tuesday.at(SCHEDULE_TIME).do(send_message, config['TUE_ROUTINE'])
 schedule.every().wednesday.at(SCHEDULE_TIME).do(send_message, config['WED_ROUTINE'])
 schedule.every().thursday.at(SCHEDULE_TIME).do(send_message, config['THU_ROUTINE'])
+schedule.every().friday.at(SCHEDULE_TIME).do(send_message, config['FRI_MSG'])
+
+# Sleep until schedule time is reached
+def sleep_until(hour, minute):
+    t = datetime.datetime.now()
+    future = datetime.datetime(t.year, t.month, t.day, hour, minute)
+    if t.hour >= hour:
+        future += datetime.timedelta(days=1)
+    print((future - t).total_seconds())
+    time.sleep((future - t).total_seconds())
 
 def main():
     while True:
+        schedule_hour = int(SCHEDULE_TIME.split(':')[0])
+        schedule_min = int(SCHEDULE_TIME.split(':')[1])
+
+        sleep_until(schedule_hour, schedule_min)
         schedule.run_pending()
-        time.sleep(1)
 
 
 if __name__ == "__main__":
